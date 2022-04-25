e2e378df4e5123cdd1454e35e1f3fba8e8355086
Marcela <mavia2@gmail.com>
2022-04-25 09:17:08 -0300

    proyectofinalhtml

---
+++ objetos.ts
@@ -0,0 +1,13 @@
+interface Persona{
+    Nombre: string;
+    Apellido: string;
+    Acercade: string;
+    Titulos: [string, string]
+}
+
+let porfolio: Persona={
+    Nombre:``,
+    Apellido:``,
+    Acercade:``,
+    Titulos:[``, ``],
+}
